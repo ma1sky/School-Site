f73e6e489439e8557ec2e65b64e6d6ee454bec60
ma1sky <ijezz@List.ru>
2023-01-06 18:24:49 +0200

    Second-block-interaction

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-let header__item = document.querySelectorAll(".header__item");
 
-for (let item of header__item) {
-    item.addEventListener("click", function(){
-        for (let other of header__item) {
+document.querySelectorAll(".header__item").forEach(item => {
+    item.onclick = () => {
+        document.querySelectorAll(".header__item").forEach(other => {
             other.classList.remove("header__item_active");
-        }
-        this.classList.add("header__item_active");
-    });
-};
+        });
+        item.classList.add("header__item_active");
+    };
+});
+
+document.querySelectorAll(".article-news").forEach(item => {
+    item.onmouseenter = () => {
+        item.childNodes[3].childNodes[1].classList.add("article-news_hover");
+        item.childNodes[1].classList.add("article-news__image_hover");
+    }
+    item.onmouseleave = () => {
+        item.childNodes[3].childNodes[1].classList.remove("article-news_hover");
+        item.childNodes[1].classList.remove("article-news__image_hover");
+    }
+});
+
+document.querySelectorAll(".article-news_small").forEach(item => {
+    item.onmouseenter = () => {
+        item.childNodes[1].childNodes[1].classList.add("article-news_hover");
+    }
+    item.onmouseleave = () => {
+        item.childNodes[1].childNodes[1].classList.remove("article-news_hover");
+    }
+});
